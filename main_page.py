b7e191f8ff4da11709638894ea0ecf1861fe1c07
unknown <cross054@naver.com>
2021-07-11 13:39:08 +0900

    Add button in RAM allocation menu

--- main_page.py
+++ main_page.py
@@ -1,18 +1,17 @@
 import os
 import wx
 
-
 class MainFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(800,600))
         menu_bar = wx.MenuBar()
         menu = wx.Menu()
-        menu_bar.Append(menu, 'Setting')
+        menu_bar.Append(menu, 'Menu')
         menu.Append(101,'Adjust RAM allocation')
-        self.SetMenuBar(menu_bar)
         self.Bind(wx.EVT_MENU, self.RAM_allocation, id=101)
         menu.Append(102, 'Quit')
         self.Bind(wx.EVT_MENU, self.Quit, id=102)
+        self.SetMenuBar(menu_bar)
 
     def RAM_allocation(self,event):
         print('RAM allocation activated.')
@@ -32,14 +31,20 @@ class MainFrame(wx.Frame):
 class CheckFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(800,600))
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.button = wx.Button(self, id=1, label = '1') #parent, id(ID_ANY), label, pos, size, style, validator, name
         self.Bind(wx.EVT_BUTTON, self.ButtonClick, id=1) #event, handler, source, id
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self.button, 0, wx.ALL, 5)
+        self.button2 = wx.Button(self, id=2, label = '2')
+        self.Bind(wx.EVT_BUTTON, self.ButtonClick2, id=2)
+        sizer.Add(self.button2, 0, wx.ALL, 5)
         self.SetSizer(sizer)
 
     def ButtonClick(self, event):
         print('Button 1 clicked')
+    
+    def ButtonClick2(self, event):
+        print('Button 2 clicked')
 
 
 def run():
