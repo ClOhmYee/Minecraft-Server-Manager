030a71bafd6816742f76009a4cdee213d7084400
unknown <cross054@naver.com>
2021-07-11 13:23:02 +0900

    Add quit menu / Test button in RAM allocation menu

--- main_page.py
+++ main_page.py
@@ -11,18 +11,35 @@ class MainFrame(wx.Frame):
         menu.Append(101,'Adjust RAM allocation')
         self.SetMenuBar(menu_bar)
         self.Bind(wx.EVT_MENU, self.RAM_allocation, id=101)
+        menu.Append(102, 'Quit')
+        self.Bind(wx.EVT_MENU, self.Quit, id=102)
 
     def RAM_allocation(self,event):
         print('RAM allocation activated.')
         frame = CheckFrame(self, -1, 'Server RAM allocation')
         frame.Show()
 
+    def Quit(self, event):
+        dialog = wx.MessageDialog(self, 'Are you sure want to quit?', 'Warning', wx.CANCEL)
+        if dialog.ShowModal() == wx.ID_OK:
+            quit(0)
+        else:
+            pass
+        dialog.Destroy()
+
 
 
 class CheckFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(800,600))
+        self.button = wx.Button(self, id=1, label = '1') #parent, id(ID_ANY), label, pos, size, style, validator, name
+        self.Bind(wx.EVT_BUTTON, self.ButtonClick, id=1) #event, handler, source, id
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.button, 0, wx.ALL, 5)
+        self.SetSizer(sizer)
 
+    def ButtonClick(self, event):
+        print('Button 1 clicked')
 
 
 def run():
