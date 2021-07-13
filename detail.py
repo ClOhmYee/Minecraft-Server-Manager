1c61864e420120126238945b6c3b0eb33d70db37
unknown <cross054@naver.com>
2021-07-13 14:21:37 +0900

    File not to be used

--- detail.py
+++ detail.py
@@ -1,12 +1,16 @@
 import wx
-import check
+from wx.core import Dialog
 
+
+
+
+# -----------------------------------xms-----------------------------------
 class DetailXmsInfo(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title, size=(500,170))
 
         self.panel = wx.Panel(self)
-        self.xms = wx.StaticText(self.panel, label = "Please enter the minimal RAM allocation you want (unit : MB) :")
+        self.xms = wx.StaticText(self.panel, label = "Please enter the minimum RAM allocation you want (unit : MB) :")
         self.xms.SetPosition((10,50))
         self.g_xms = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
         self.g_xms.SetPosition((365,50))
@@ -24,13 +28,53 @@ class DetailXmsInfo(wx.Frame):
         print(xms)
         dialog = wx.MessageDialog(self, 'Are you sure want to allocate your server minimum RAM : {}MB?'.format(xms_value), 'Warning', wx.CANCEL)
         if dialog.ShowModal() == wx.ID_OK:
-            check.xms = xms
+            SendXmsData()
             self.Close() # close the detail page
         else:
             pass
-        print(xms)
+        Dialog.Destroy()
 
-        
+def SendXmsData():
+    import check
+    check.xms = xms
+
+
+
+
+# -----------------------------------xmx-----------------------------------
+class DetailXmxInfo(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(500,170))
+
+        self.panel = wx.Panel(self)
+        self.xmx = wx.StaticText(self.panel, label = "Please enter the maximum RAM allocation you want (unit : MB) :")
+        self.xmx.SetPosition((10,50))
+        self.g_xmx = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
+        self.g_xmx.SetPosition((365,50))
+
+        self.button = wx.Button(self.panel, label = "Done")
+        self.button.SetPosition((400,100))
+        self.button.Bind(wx.EVT_BUTTON, self.ButtonClick_2)
+        self.g_xmx.Bind(wx.EVT_TEXT_ENTER, self.ButtonClick_2)
+
+    
+    def ButtonClick_2(self,event):
+        global xmx
+        xmx_value = self.g_xmx.GetValue()
+        xmx = '-Xmx'+str(xmx_value)+'M'
+        print(xmx)
+        dialog = wx.MessageDialog(self, 'Are you sure want to allocate your server maximum RAM : {}MB?'.format(xmx_value), 'Warning', wx.CANCEL)
+        if dialog.ShowModal() == wx.ID_OK:
+            SendXmxData()
+            self.Close() # close the detail page
+        else:
+            pass
+        Dialog.Destroy()
+    
+def SendXmxData():
+    import check
+    check.xmx = xmx
+
 
 
 # def run():
