bccdfe24d4e336cbb47fb91c3f61fef3ac793060
unknown <cross054@naver.com>
2021-07-17 13:48:14 +0900

    Change dir to start the server with any condition

--- start_server.py
+++ start_server.py
@@ -2,6 +2,7 @@ import os, wx
 import DataTool
 
 app = wx.App()
+os.chdir(DataTool.base_path)
 
 def RunServer():
     try: # import all data to boot server
@@ -19,6 +20,24 @@ def RunServer():
         dialog.ShowModal()
         dialog.Destroy()
     else:
-        cmd = 'java {} {} -jar {}'.format(xms, xmx, jar) 
+        cmd = 'java {} {} -jar {}'.format(xms, xmx, jar)
         print('Starting the server . . .')
+        print(cmd)
         os.system(cmd)
+
+RunServer()
+
+
+# file_xms = open(DataTool.data_path+'\data_xms.txt','r')
+# xms = file_xms.read()
+# file_xms.close()
+# file_xmx = open(DataTool.data_path+'\data_xmx.txt','r')
+# xmx = file_xmx.read()
+# file_xmx.close()
+# file_jar = open(DataTool.data_path+'\data_jar.txt','r')
+# jar = file_jar.read()
+# file_jar.close()
+# cmd = 'java -Xms8192M -Xmx8192M -jar bukkit.jar nogui'
+# print('Starting the server . . .')
+# print(cmd)
+# os.system(cmd)
