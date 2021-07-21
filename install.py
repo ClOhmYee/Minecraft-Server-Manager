b39bc3b1d9d1d97e4da5907373fd953e760a9a41
BuildTools <cross054@naver.com>
2021-07-21 18:14:23 +0900

    Final version of downloading process

--- install.py
+++ install.py
@@ -4,25 +4,30 @@ import wx
 
 
 
-os.system('curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar')
-
 class InstallFrame(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(450,70))
+        wx.Frame.__init__(self, parent, id, title, size=(500,150))
         self.version = wx.StaticText(self, label = 'Enter the version of the server you want to start : ')
-        self.version.SetPosition((5,10))
+        self.version.SetPosition((10,15))
         self.g_version = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-        self.g_version.SetPosition((280,7))
+        self.g_version.SetPosition((280,12))
         self.button = wx.Button(self, label = 'Done')
-        self.button.SetPosition((150,70))
+        self.button.SetPosition((400,12))
         self.button.Bind(wx.EVT_BUTTON, self.ButtonClick)
         self.g_version.Bind(wx.EVT_TEXT_ENTER, self.ButtonClick)
+        self.warning = wx.StaticText(self, label = 'Once you have completed entering the version, you will download server tools. \nPlease DO NOT MANIPULATE the program until you receive a notification message.')
+        self.warning.SetPosition((10,70))
+
+        self.Centre()
 
     def ButtonClick(self, event):
         dialog = wx.MessageDialog(self, 'Are you sure want to configure the server for version {}?'.format(self.g_version.GetValue()), 'Warning', wx.CANCEL)
         if dialog.ShowModal() == wx.ID_OK:
             version_value = self.g_version.GetValue()
+            os.system('curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar')
             os.system('java -jar BuildTools.jar --rev {}'.format(version_value))
+            dialog = wx.MessageDialog(self, 'Download complete.', 'Notification', wx.OK)
+            dialog.ShowModal()
             self.Close() # close page
         else:
             pass
@@ -32,8 +37,8 @@ class InstallFrame(wx.Frame):
 def RunInstall():
     app = wx.App()
     frame = InstallFrame(None, -1, 'Installation')
-    frame.SetMaxSize(wx.Size(450,70))
-    frame.SetMinSize(wx.Size(450,70))
+    frame.SetMaxSize(wx.Size(450,150))
+    frame.SetMinSize(wx.Size(450,150))
     frame.Show()
     app.MainLoop()
 
