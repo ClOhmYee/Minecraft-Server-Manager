bc6071742e5181fd69ccaa5910eab5b953fb8720
BuildTools <cross054@naver.com>
2021-07-21 17:39:18 +0900

    Add frame to input version, then download server tools (unfinished)

--- install.py
+++ install.py
@@ -1,9 +1,40 @@
 import os
+import wx
 
 
-def BuildTools():
-    os.system('curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar')
-    os.system('set /p Data=Enter the version (1.16.5, 1.17.1, etc) : || set Data=latest')
-    os.system('java -jar BuildTools.jar --rev %Data%')
 
-BuildTools()
+
+os.system('curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar')
+
+class InstallFrame(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(450,70))
+        self.version = wx.StaticText(self, label = 'Enter the version of the server you want to start : ')
+        self.version.SetPosition((5,10))
+        self.g_version = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        self.g_version.SetPosition((280,7))
+        self.button = wx.Button(self, label = 'Done')
+        self.button.SetPosition((150,70))
+        self.button.Bind(wx.EVT_BUTTON, self.ButtonClick)
+        self.g_version.Bind(wx.EVT_TEXT_ENTER, self.ButtonClick)
+
+    def ButtonClick(self, event):
+        dialog = wx.MessageDialog(self, 'Are you sure want to configure the server for version {}?'.format(self.g_version.GetValue()), 'Warning', wx.CANCEL)
+        if dialog.ShowModal() == wx.ID_OK:
+            version_value = self.g_version.GetValue()
+            os.system('java -jar BuildTools.jar --rev {}'.format(version_value))
+            self.Close() # close page
+        else:
+            pass
+        dialog.Destroy()
+
+
+def RunInstall():
+    app = wx.App()
+    frame = InstallFrame(None, -1, 'Installation')
+    frame.SetMaxSize(wx.Size(450,70))
+    frame.SetMinSize(wx.Size(450,70))
+    frame.Show()
+    app.MainLoop()
+
+RunInstall()
